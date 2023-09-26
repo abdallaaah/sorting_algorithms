5e0e54663bdf3beac0cd1ce2b3fb7709dc7ac931
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-25 17:38:06 -0700

    check task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,24 +2,28 @@
 /**
  * bubble_sort - bubble sort algotithm in list
  * @array: array to be soreted
- * @sizeo: size of the array
+ * @size: size of the array
  * Return: void
  */
-void bubble_sort(int *array, size_t sizeo)
+void bubble_sort(int *array, size_t size)
 {
 size_t i = 0;
 size_t x = 0;
 int temp = 0;
-for (i = 0; i < sizeo - 1; i++)
+if (array == NULL || size < 2)
 {
-for (x = 0; x < sizeo - 1; x++)
+return;
+}
+for (i = 0; i < size - 1; i++)
+{
+for (x = 0; x < size - 1; x++)
 {
 if (array[x] > array[x + 1])
 {
 temp = array[x];
 array[x] = array[x + 1];
 array[x + 1] = temp;
-print_array(array, sizeo);
+print_array(array, size);
 }
 }
 }
